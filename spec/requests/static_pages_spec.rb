3dc95b00fde3939824d0611ce7b2eccb97e7be8e
mnxoid <sergolwow@gmail.com>
2015-09-27 18:36:43 +0300

    Static pages completed (Issue #4): still no index

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,6 +18,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Mnxoid home')
     end
+    it "should have the right title" do
+  	  visit '/static_pages/home'
+  	  expect(page).to have_title('Rubysite | Home')
+  	end
   end
 
   describe "Help page" do
@@ -25,6 +29,21 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the right title" do
+  	  visit '/static_pages/help'
+  	  expect(page).to have_title('Rubysite | Help')
+  	end
   end
-  
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the right title" do
+  	  visit '/static_pages/about'
+  	  expect(page).to have_title('Rubysite | About')
+  	end
+  end
+
 end
